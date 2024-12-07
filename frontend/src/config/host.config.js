8c9167a620280bdee338657b0086b657626f49f2
PetroPetrenko <petrpetrenko2023@gmail.com>
2024-12-07 07:54:35 +0200

    Update host.config.js

--- frontend/src/config/host.config.js
+++ frontend/src/config/host.config.js
@@ -19,14 +19,14 @@ const hostConfig = {
   },
   production: {
     frontend: {
-      host: import.meta.env.VITE_FRONTEND_URL || 'your-frontend-url.vercel.app',
+      host: import.meta.env.VITE_FRONTEND_URL || 'https://chat-reenbit2024-9pdv.vercel.app/',
       protocol: 'https',
       get url() {
         return `${this.protocol}://${this.host}`;
       }
     },
     backend: {
-      host: import.meta.env.VITE_API_URL || 'your-backend-url.vercel.app',
+      host: import.meta.env.VITE_API_URL || 'https://chat-reenbit2024-m64f-n95u8gec4-pedros-projects-06014566.vercel.app',
       protocol: 'https',
       get url() {
         return `${this.protocol}://${this.host}`;
