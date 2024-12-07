e424dab2c9f9263739d47eaf78ea5a1537f513a8
PetroPetrenko <petrpetrenko2023@gmail.com>
2024-12-07 07:15:57 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   build: {
+    rollupOptions: {
+      external: ['prop-types']
+    }
+  },
   plugins: [react()],
   server: {
     port: 5173,
